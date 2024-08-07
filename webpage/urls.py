b79e607ed505f3a8449e5fbef2afed7b10ff2dd4
deaww77 <expchikgen@gmail.com>
2024-08-07 09:02:23 +0700

    add forPage

--- webpage/urls.py
+++ webpage/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns = [
     path("",  views.indexPage, name="index"),
     path("about/", views.aboutUs, name="aboutUs"),
-    path("contact/", views.contactUs, name="contactUs")
+    path("contact/", views.contactUs, name="contactUs"),
+    path("for/", views.forPage, name="for_page")
 ]
